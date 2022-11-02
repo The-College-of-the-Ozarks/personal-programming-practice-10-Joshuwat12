f186c6acbeebed942a68fd85c0c0043c78b36187
Joshua <72043974+Joshuwat12@users.noreply.github.com>
2022-11-02 19:36:04 +0000

    Complete work

--- handshake.py
+++ handshake.py
@@ -1,2 +1,20 @@
 # handshake.py
-#
+
+
+# Function
+def handshakes(n):
+  # Bonus
+  if not isinstance(n, int):
+    raise TypeError("Integers only")
+  if n < 1:
+    raise ValueError("Positive integers only")
+  
+  if n < 2:
+    return 0
+  return n - 1 + handshakes(n-1)
+
+
+# Main program
+people = int(input("Enter a natural number of people: "))
+h = handshakes(people)
+print(h, "handshakes were exchanged.")
--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,18 @@
 # palindrome.py
-#
 
+
+# Function
+def isPalindrome(string):
+  if len(string) < 2:
+    return True
+  elif string[0] == string[-1]:
+    return isPalindrome(string[1:-1])
+  return False
+
+
+# Main program
+string = input("Enter a string: ")
+if isPalindrome(string):
+  print(string, "is a palindrome.")
+else:
+  print(string, "is not a palindrome.")
